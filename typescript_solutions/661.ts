9490bea44e51d4a5845fb3ad18c0c90fdda8c90f
Matias Brosens <mathias.brosens@student.kuleuven.be>
2023-12-19 09:07:59 +0100

    Daily challenge dec19

---
+++ typescript_solutions/661.ts
@@ -0,0 +1,57 @@
+/**
+ * @author  Mathias Brosens
+ * @date    2023-12-19
+ */
+function imageSmoother(img: number[][]): number[][] {
+  const result: number[][] = [];
+  for (let i = 0; i < img.length; i++) {
+    result.push([]);
+    for (let j = 0; j < img[0].length; j++) {
+      let n = 1;
+      let sum: number = img[i][j];
+      if (i + 1 < img.length) {
+        sum += img[i + 1][j];
+        n++;
+      }
+      if (i - 1 >= 0) {
+        sum += img[i - 1][j];
+        n++;
+      }
+      if (j + 1 < img[0].length) {
+        sum += img[i][j + 1];
+        n++;
+      }
+      if (j - 1 >= 0) {
+        sum += img[i][j - 1];
+        n++;
+      }
+      if (i + 1 < img.length && j + 1 < img[0].length) {
+        sum += img[i + 1][j + 1];
+        n++;
+      }
+      if (i + 1 < img.length && j - 1 >= 0) {
+        sum += img[i + 1][j - 1];
+        n++;
+      }
+      if (i - 1 >= 0 && j + 1 < img[0].length) {
+        sum += img[i - 1][j + 1];
+        n++;
+      }
+      if (i - 1 >= 0 && j - 1 >= 0) {
+        sum += img[i - 1][j - 1];
+        n++;
+      }
+
+      result[i].push(Math.floor(sum / n));
+    }
+  }
+  return result;
+}
+
+console.log(
+  imageSmoother([
+    [1, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1],
+  ])
+);
